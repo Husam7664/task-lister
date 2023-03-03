51e4495401e05e757a97ddd58ede921ff1a450f2
Husam7664 <husam7664@gmail.com>
2023-03-04 00:14:04 +0530

    backend server create and mongodb conected

---
+++ backend/database/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const Connection = async () => {
+  const URL = `mongodb://hn:test123@ac-sckd5d6-shard-00-00.3bcbrsm.mongodb.net:27017,ac-sckd5d6-shard-00-01.3bcbrsm.mongodb.net:27017,ac-sckd5d6-shard-00-02.3bcbrsm.mongodb.net:27017/?ssl=true&replicaSet=atlas-mo3bkp-shard-0&authSource=admin&retryWrites=true&w=majority`
+  try {
+    await mongoose.connect(URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    console.log('Database Connected Succesfully')
+  } catch (error) {
+    console.log('Error while connecting with the database', error)
+  }
+}
+
+export default Connection
