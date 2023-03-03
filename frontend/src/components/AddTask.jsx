41dddb835d13922e53ee5f19eb7563e576811bdb
Husam7664 <husam7664@gmail.com>
2023-03-03 21:37:35 +0530

    imporove ad task cpomnetn - date picker, dropdown

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -5,32 +5,52 @@ import {
   InputLabel,
   Typography,
   styled,
+  Button,
+  Select,
+  MenuItem,
 } from '@mui/material'
 
 const Container = styled(FormGroup)`
   width: 50%;
-  margin: 5% auto 0 auto ;
+  margin: 5% auto 0 auto;
+  & div {
+    margin-top: 20px;
+  }
 `
+const onValueChange = (e) => {
+  console.log(e.target.name, e.target.value)
+}
 
 const AddTask = () => {
   return (
     <Container>
-      <Typography variant='h4'>Add User</Typography>
+      <Typography variant='h4'>Add Task</Typography>
       <FormControl>
         <InputLabel>title</InputLabel>
-        <Input />
+        <Input onChange={(e) => onValueChange(e)} name='title' />
       </FormControl>
       <FormControl>
         <InputLabel>description</InputLabel>
-        <Input />
+        <Input onChange={(e) => onValueChange(e)} name='description' />
       </FormControl>
-      <FormControl>
-        <InputLabel>due date</InputLabel>
-        <Input />
+      <FormControl variant='standard'>
+        <Input
+          name='someDate'
+          label='Some Date'
+          InputLabelProps={{ shrink: true, required: true }}
+          type='date'
+          onChange={(e) => onValueChange(e)}
+        />
       </FormControl>
-      <FormControl>
+      <FormControl variant='standard'>
         <InputLabel>completion status</InputLabel>
-        <Input />
+        <Select onChange={(e) => onValueChange(e)} name='completionStatus'>
+          <MenuItem value={true}>Yes</MenuItem>
+          <MenuItem value={false}>No</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl>
+        <Button variant='contained'>Add Task</Button>
       </FormControl>
     </Container>
   )
